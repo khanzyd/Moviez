76bd8c8c5da4401444a49f34877097fdc23d53bb
khanzyd <khanzyd7@gmail.com>
2024-01-26 23:43:08 +0530

    added search bar in navbar with animation

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import {} from "react-router-dom";
 
 import heroSectionBanner from "../assets/avengers-end-game.jpg";
+import { FaPlay } from "react-icons/fa";
+
 
 const HeroSection = () => {
   return (
@@ -17,8 +19,40 @@ const HeroSection = () => {
           <div className="absolute z-10 img-container h-full w-10/12 right-0 shadow-[inset_350px_0px_190px_50px_black]"></div>
         </div>
 
-        <div className="relative z-40 top-28 px-10 text-slate-100 ">
-          HeroSection
+        <div className="relative z-40 top-32 px-10 text-slate-100 ">
+          <span className="rounded-full text-red-600 border-2 border-slate-100 px-3.5 py-2 text-lg font-bold select-none">
+            <span className="text-slate-100">#</span>Trending🔥
+          </span>
+
+          <div className="h-56 w-[50%] mt-36 ">
+            <button className="rounded-full border-2 border-slate-100 px-3.5 py-1 mr-5 text-sm font-semibold">
+              Animation
+            </button>
+            <button className="rounded-full border-2 border-slate-100 px-3.5 py-1 text-sm font-semibold">
+              Sci-Fi
+            </button>
+            <div className="h-auto py-6">
+              <h2 className="text-6xl font-semibold">
+                spider-man : Across the spider verse
+              </h2>
+              <p className="my-5 font-medium text tracking-wide">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam
+                nisi at esse voluptatibus sequi, itaque suscipit! Distinctio, ab
+                recusandae sapiente aut in natus sit necessitatibus esse.
+                Ducimus atque repellat at, culpa soluta dicta magni enim in
+                facilis eum perferendis incidunt natus quam mollitia qui?
+              </p>
+              <button className="text-xl font-semibold border-2 border-transparent text-black bg-slate-100 rounded-full px-5 py-1 mr-7 hover:border-2 hover:text-slate-100 hover:bg-black hover:border-slate-100 inline-flex justify-center items-center">
+                <span>
+                  <FaPlay className="mr-2 text-lg" />
+                </span>
+                Trailer
+              </button>
+              <button className="text-xl font-medium border-2 border-slate-100 rounded-full px-5 py-1">
+                Details
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,78 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import gsap, { Power3 } from "gsap";
 import { NavLink } from "react-router-dom";
 
+import { HiSearch } from "react-icons/hi";
+
 const Navbar = () => {
+  let [height, setHeight] = useState(0);
+  let [searching, setSearching] = useState(false);
+  let searchForm = useRef(null);
+  let formOpenbutton = useRef(null);
+  let formInput = useRef(null);
+
+  let openForm = () => {
+    let tl = gsap.timeline();
+
+    tl.set(formOpenbutton, {
+      display: "none",
+    }).set(searchForm, {
+      display: "block",
+    }).fromTo(
+      searchForm,{
+        x: 200,
+        width: 0,
+        height: 0,
+      },
+      {
+        width: "100%",
+        height: "100%",
+        x: 0,
+        duration: 0.75,
+        ease: Power3.easeInOut,
+      });
+  };
+
+  useEffect(() => {
+
+  }, []);
+
   return (
     <>
-      <div className="navbar z-50 h-28 w-full px-10 flex items-center text-slate-50 absolute">
-        <NavLink
-        className={"text-xl font-bold"} 
-        to="/"
+      <div className="navbar z-50 h-28 w-full px-10 flex items-center justify-between text-slate-50 absolute">
+        <div>
+          <NavLink className={"text-xl font-bold"} to="/">
+            Home
+          </NavLink>
+        </div>
 
-        >Home</NavLink>
+        <div className="flex justify-center items-center p-2">
+          <button
+            className="border-2 rounded-[100%] py-2 px-3"
+            ref={(el) => {
+              formOpenbutton = el;
+            }}
+            onClick={openForm}
+          >
+            <HiSearch className="text-xl" />
+          </button>
+
+          <div
+            className="justify-center items-center overflow-hidden hidden"
+            ref={(el) => {
+              searchForm = el;
+            }}
+          >
+            <input
+              className="bg-slate-200 rounded-full px-4 py-1 text-gray-900 font-semibold tracking-wider selection:bg-neutral-900 selection:text-slate-100 outline-none "
+              type="text"
+              placeholder="Search"
+              ref={(el) => {
+                formInput = el;
+              }}
+            />
+          </div>
+        </div>
       </div>
     </>
   );
